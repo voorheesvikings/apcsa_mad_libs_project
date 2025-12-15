a9097f76e3ffa7a3267c2e3c9add04f11cb4df97
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-12-15 13:37:59 +0000

    feat: madlib func create

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    String loadMadLib(String n1, String n2, String adj1, String v1, String n3, String v2, String adj2, String adj3, String n4, String n5) {
+        String madLib = "One " + n1 + " named " + n2 + " was a very\n " + adj1 + " chum. He moped and " + v1 + " a lot, in \n fact, they called him a big " + n3 + ". Then one day, he\n " + v2 + " to school he was " + adj2 + " all the way.\n Though now he was " + adj3 + ". Yayyyyy. Santa came on his \n" + n4 + " with many many " + n5 + " and with all these\n gifts recieved, we all smiled in glee.";
+
+    }
+
+}
